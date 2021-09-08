e27ec7770563013a6669927e5c23b5dc6917cd0d
AudricSkivée <audricskivee@gmail.com>
2021-09-08 17:15:08 +0200

    still reparing my shit

--- src/frontpage.php
+++ src/frontpage.php
@@ -409,7 +409,47 @@
         </aside>
     </main>
     <footer>
-        footer
+        <div class="gradient-custom-footer">
+            <div class="d-flex flex-wrap justify-content-center">
+                <a class="social d-flex justify-content-center align-items-center" href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="https://www.apple.com/" target="_blank"><i class="fab fa-apple"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="https://codepen.io/" target="_blank"><i class="fab fa-codepen"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="#"><i class="fab fa-google-plus-g"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="https://digg.com/" target="_blank"><i class="fab fa-digg"></i></a>
+                <a class="social d-flex justify-content-center align-items-center" href="https://www.pinterest.com/" target="_blank"><i class="fab fa-pinterest-p"></i></a>
+            </div>
+        </div>
+        <div class="foot-info">
+            <div class="d-flex flex-nowrap justify-content-between align-items-center">
+                <nav class="nav-footer" style="--bs-breadcrumb-divider: '<';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a class="nav-breadcrumb" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                    </svg>Home</a></li>
+                    <li class="breadcrumb-item"><a class="nav-breadcrumb" href="#">Topics</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Comments</li>
+                    </ol>
+                </nav>
+                <div class="d-flex flex-wrap" id="list-foot">
+                    <a class="foot-contact" href="#"><i class="fas fa-envelope"></i>Contact us</a>
+                    <a class="foot-contact" href="#"><i class="fas fa-shield-alt"></i>The team</a>
+                    <a class="foot-contact" href="#"><i class="fas fa-check"></i>Terms</a>
+                    <a class="foot-contact" href="#"><i class="fas fa-lock"></i>Privacy</a>
+                    <a class="foot-contact" href="#"><i class="fas fa-users"></i>Members</a>
+                    <a class="foot-contact" href="#"><i class="fas fa-trash-alt"></i>Delete cookies</a>
+                    <p class="time">All times are UTC</p>
+                </div>
+            </div>
+        </div>
+        <div class="credit-foot">
+            <div class="d-flex flex-wrap">
+                <p class="credits">Powered by <a href="#">phpBB™</a> • </p>
+                <p class="credits">Design by <a href="#">PlanetStyles</a></p>
+            </div>
+        </div>
     </footer>
     <script src="dist/main.js"></script>
 </body>
